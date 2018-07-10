844c3d18997841dea43398ab2362ee8fa9387d52
wandesky <bwandesky@gmail.com>
2018-07-10 10:35:13 +0300

    add unit test for accessing all rides

--- test.py
+++ test.py
@@ -1,3 +1,26 @@
+from app import app
+import unittest
+
+
+class FlaskTestCase(unittest.TestCase):
+
+    # # Ensure that flask was set up correctly
+    # def test_index(self):
+    #     tester = app.test_client(self)
+    #     response = tester.get('/login', content_type='html/text')
+    #     self.assertEqual(response.status_code, 200)
+
+# Ensure that all rides are displayed
+    def test_acess_all_rides(self):
+        tester = app.test_client(self)
+        response = tester.get('/api/v1/rides', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
 ###from models import User, Driver, Passenger
 
 # content of test_class.py
